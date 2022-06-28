723f5ca1a409abcb470746d44805c7da50fee35c
bilalkhan891 <bilalkhan891@hotmail.com>
2022-06-29 01:41:50 +0500

    Redux implementation

---
+++ client/src/components/auth/Login.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const [formData, setFormDate] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const handleChange = (e) => (e) => {
+    setFormDate((formData) => ({
+      ...formData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const newUser = {
+        email,
+        password,
+      };
+      const config = {
+        header: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await axios.post("/api/users", newUser, config);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.request.response);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Login to Your Account
+      </p>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            onChange={handleChange()}
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            onChange={handleChange()}
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/regiter">Register</Link>
+      </p>
+    </div>
+  );
+}
+
+export default Login;
