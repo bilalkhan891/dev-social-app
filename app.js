e76f65ccc2385a414a4a94c7999191de1e9b1160
Bilal Ahmad Afridi <bilalkhan891@hotmail.com>
2022-07-07 05:02:32 +0500

    make it better

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -22,10 +23,17 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+//  Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  utils.init();
-  console.log(utils.init());
   console.log(
     `Backend Listening on Port ${port}`,
     `Local: http://localhost:${port}`
